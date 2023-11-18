fdc22d6b98ec699c0e52b59226e8202279ca495e
Tim Raveling <tsraveling@gmail.com>
2023-11-17 17:32:27 -0700

    Completed export

--- scenes/roulette/RouletteScene.gd
+++ scenes/roulette/RouletteScene.gd
@@ -4,6 +4,10 @@ extends Control
 @onready var image_count_label := $VerticalLayout/ToolBar/ImageCountLabel
 @onready var current_path_label := $VerticalLayout/ToolBar/CurrentPathLabel
 
+# STUB: On pick, pop the picked image and put it in a "done" array
+# STUB: Add timers
+# STUB: Clicking a checkbox on or off
+
 func get_next():
 	if FileBrowser.image_paths.size() == 0:
 		image_rect.texture = null
@@ -12,13 +16,12 @@ func get_next():
 	
 	var image_index = randi() % FileBrowser.image_paths.size()
 	var image_path = FileBrowser.image_paths[image_index]
+	var disp_path = image_path.replace(FileBrowser.root_directory, "")
 	var image = Image.load_from_file(image_path)
 	if image != null:
 		var texture = ImageTexture.create_from_image(image)
 		image_rect.texture = texture
-		current_path_label.text = image_path
-	else:
-		current_path_label.text = "INVALID IMAGE: %s" % image_path
+		current_path_label.text = disp_path
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
